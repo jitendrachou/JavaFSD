b837c54bed96374d7f18b664ac566acc85f253f2
Jitendra Choudhary <jitendrachou835103@gmail.com>
2022-04-12 15:27:33 +0530

    first checkin

---
+++ Project1/src/myPackage/MyClass1.java
@@ -0,0 +1,16 @@
+package myPackage;
+
+public class MyClass1
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+		int a=5;
+		int b=a++;
+		System.out.println(a);
+		System.out.println(b);
+	}
+
+}
